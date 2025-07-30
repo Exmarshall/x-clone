c7ee1c7f216b4850da37e8de2a4665aac3b4e97d
Exmarshall <umarhayatudeen88@gmail.com>
2025-07-30 06:27:19 -0700

    db models  created

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,13 @@
-import express from 'express'
+import express from "express";
+import { ENV } from "./config/env.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is up and Running on PORT:5001"))
+connectDB();
+
+app.get("/", (req, res) => res.send("Hello from MARSHALL"));
+
+app.listen(ENV.PORT, () =>
+  console.log("Server is up and Running on PORT:", ENV.PORT)
+);
